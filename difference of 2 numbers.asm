7a4f17f63d405ed431b9bb16f8107266af55ced7
revathi2001 <54894782+revathi2001@users.noreply.github.com>
2021-01-24 15:10:03 +0530

    Create difference of 2 numbers.asm

---
+++ difference of 2 numbers.asm
@@ -0,0 +1,54 @@
+BITS 32
+
+extern printf
+extern scanf
+
+section .data
+	fmt: db "%d", 0
+	output: db "Difference is %d",10,0
+	msg1 db 'Enter a input value',0xa
+	len1 equ $ - msg1
+	msg2 db 'Enter other input value',0xa
+	len2 equ $ - msg2
+
+section .text
+	global main
+
+	main:
+	mov	edx,len1     
+   	mov	ecx,msg1     
+   	mov	ebx,1       
+   	mov	eax,4       
+   	int	0x80
+
+	push ebp
+	mov ebp, esp
+	sub esp, 0x10
+
+	lea eax, [ebp-0x4]
+	push eax
+	push fmt
+	call scanf
+
+	mov	edx,len2     
+   	mov	ecx,msg2     
+   	mov	ebx,1       
+   	mov	eax,4       
+   	int	0x80
+
+	lea edx, [ebp-0x8]
+	push edx
+	push fmt
+	call scanf
+
+	mov eax, dword [ebp-0x4]
+	mov ebx, dword [ebp-0x8]
+	sub eax, ebx
+	mov dword [ebp-0x8], eax
+
+	push dword [ebp-0x8]
+	push output
+	call printf
+
+	leave
+	ret
